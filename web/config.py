ed624295b90f64638762782715c6c278e5b7895d
VuHaiDang <dangvh@gemvietnam.com>
2024-10-02 14:36:52 +0700

    push new config

--- web/config.py
+++ web/config.py
@@ -4,7 +4,7 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL="dangvh8server.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_URL="dangvh8server2.postgres.database.azure.com"  #TODO: Update value
     POSTGRES_USER="dangvh8admin" #TODO: Update value
     POSTGRES_PW="Thehangedman!2"   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
@@ -12,7 +12,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://dangvh8.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=NeCY8kYs3UEBROUiWDqIi/+ZXqKNb3sPL+ASbOKcKKA=' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://dangvh8sb2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=s9tZdBj0g5RLQGY4QR75E5mRQdwOVv/4t+ASbBcEmtA=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
